e1f6fc2919dd8001632177665b9c34bdd54332e0
kriptonian <shymal.sawan@gmail.com>
2023-04-16 19:00:09 +0530

    feat: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            {console.log(
+                '%cMade with ❤️ by https://zenversee.com',
+                'color: #D9D9D9; font-size: 16px; background: #101010; padding: 10px;',
+            )}
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className='bg-[#111111] overflow-x-hidden'>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            {/* Made with ❤️ by https://zenversee.com */}
+            <Head />
+            <body className="bg-[#111111] overflow-x-hidden">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
